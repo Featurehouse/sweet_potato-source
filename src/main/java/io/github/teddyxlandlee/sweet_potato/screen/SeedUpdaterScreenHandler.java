2649e21dae2d4c6a212b086d2cf2a634d3c03326
teddyxlandlee <tedexe_work.top@foxmail.com>
2020-12-30 20:24:29 +0800

    @ School !!!

--- src/main/java/io/github/teddyxlandlee/sweet_potato/screen/SeedUpdaterScreenHandler.java
+++ src/main/java/io/github/teddyxlandlee/sweet_potato/screen/SeedUpdaterScreenHandler.java
@@ -64,8 +64,10 @@ public class SeedUpdaterScreenHandler extends ForgingScreenHandler {
         this.putStack(0);
         this.putStack(1);
         this.context.run((world1, blockPos) -> {
-            //world1.syncWorldEvent(1044/*?*/, blockPos, 0/*?*/);
-            world1.playSound(blockPos.getX(), blockPos.getY(), blockPos.getZ(), SPMMain.AGROFORESTRY_TABLE_FINISH, SoundCategory.BLOCKS, 1.0F, world1.getRandom().nextFloat() * 0.1F + 0.9F, false);
+            world1.syncWorldEvent(1044/*?*/, blockPos, 0/*?*/);
+            if (world1.isClient)
+                world1.playSound(blockPos.getX(), blockPos.getY(), blockPos.getZ(), SPMMain.AGROFORESTRY_TABLE_FINISH, SoundCategory.BLOCKS, 1.0F, world1.getRandom().nextFloat() * 0.1F + 0.9F, false);
+
         });
         return stack;
     }
