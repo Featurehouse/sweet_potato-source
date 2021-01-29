7051ba42df922637d43450f3ffd91f3cc988378c
teddyxlandlee <tedexe_work@foxmail.com>
2021-01-29 09:23:02 +0800

    Fix up code Update Yarn version

--- src/main/java/io/featurehouse/spm/util/FloatIntegerizer.java
+++ src/main/java/io/featurehouse/spm/util/FloatIntegerizer.java
@@ -10,46 +10,4 @@ public final class FloatIntegerizer {
     public static float toFloat(int i) {
         return Float.intBitsToFloat(i);
     }
-
-    /**
-     * We should test if it has error*/
-    public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Usage: java io.featurehouse.spm.util.FloatIntegerizer <--fromFloat / -FF / -TI | --toFloat / -FI / -TF> <value>");
-        } else if (args[0].toLowerCase().equals("-fromfloat") || args[0].toUpperCase().equals("-FF") || args[0].toUpperCase().equals("-TI")) {
-            float from;
-            int to;
-            float check;
-            try {
-                from = Float.parseFloat(args[1]);
-                System.out.printf("From float to int: %f to %d\n", from, (to = fromFloat(from)));
-                System.out.printf("Check: %d back to %f\n", to, (check = toFloat(to)));
-                if (from == check) {
-                    System.out.println("Program runs correctly");
-                } else {
-                    System.err.println("Program runs incorrectly");
-                }
-            } catch (NumberFormatException e) {
-                System.out.printf("%s is not a float!\n", args[1]);
-            }
-        } else if (args[0].toLowerCase().equals("-tofloat") || args[0].toUpperCase().equals("FI") || args[0].toUpperCase().equals("-TF")) {
-            int from;
-            float to;
-            int check;
-            try {
-                from = Integer.parseInt(args[1]);
-                System.out.printf("From int to float: %d to %f\n", from, (to = toFloat(from)));
-                System.out.printf("Check: %f back to %d\n", to, (check = fromFloat(to)));
-                if (from == check) {
-                    System.out.println("Program runs correctly");
-                } else {
-                    System.err.println("Program runs incorrectly");
-                }
-            } catch (NumberFormatException e) {
-                System.out.printf("%s is not an integer!\n", args[1]);
-            }
-        } else {
-            System.out.println("Usage: java io.featurehouse.spm.util.FloatIntegerizer <--fromFloat / -FF / -TI | --toFloat / -FI / -TF> <value>");
-        }
-    }
 }
--- src/main/java/io/featurehouse/spm/util/tick/ITickable.java
+++ src/main/java/io/featurehouse/spm/util/tick/ITickable.java
@@ -18,28 +18,4 @@ public interface ITickable {
             return (world, pos, state, blockEntity1) -> this.tick(world, pos, state);
         return null;
     }
-
-    @Deprecated
-    class NullTickable extends BlockEntity implements ITickable {
-
-        public NullTickable() {
-            super(null, BlockPos.ORIGIN, null);
-        }
-
-        @Override
-        public void tick(World world, BlockPos pos, BlockState state) {
-            throw new UnsupportedOperationException("Null Tickable, cannot tick!");
-        }
-
-        /** @return null indeed */
-        @Nullable
-        @Override
-        public <T extends BlockEntity> BlockEntityTicker<T> tickable2ticker() {
-            return null;
-        }
-    }
-
-    static <T extends ITickable> void iTick(World world, BlockPos pos, BlockState state, T blockEntity) {
-        blockEntity.tick(world, pos, state);
-    }
 }
