3451904240c157636a05c0639645aea71497aa76
teddyxlandlee <tedexe_work@foxmail.com>
2021-02-19 12:23:02 +0800

    Found Shurlin strings deep fix MultiMC ShurlinSPMLinkageClient @Environment

--- src/main/java/xyz/shurlin/linkage/sweet_potato/client/ShurlinSPMLinkageClient.java
+++ src/main/java/xyz/shurlin/linkage/sweet_potato/client/ShurlinSPMLinkageClient.java
@@ -1,10 +1,13 @@
 package xyz.shurlin.linkage.sweet_potato.client;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
 import xyz.shurlin.linkage.ShurlinLinkageApiClient;
 import xyz.shurlin.linkage.sweet_potato.ShurlinSPMLinkage;
 
+@Environment(EnvType.CLIENT)
 public class ShurlinSPMLinkageClient implements ShurlinLinkageApiClient {
     @Override
     public void initClient() {
