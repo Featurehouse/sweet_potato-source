33b262a08ba3621c020d5d892759f2f8c98bd722
teddyxlandlee <tedexe_work.top@foxmail.com>
2021-05-30 18:57:19 +0800

    SimpleItemPredicate.java

--- src/main/java/org/featurehouse/spm/advancement/SimpleItemPredicate.java
+++ src/main/java/org/featurehouse/spm/advancement/SimpleItemPredicate.java
@@ -1,5 +1,6 @@
 package org.featurehouse.spm.advancement;
 
+import com.google.common.collect.ImmutableSet;
 import net.minecraft.item.Item;
 import net.minecraft.predicate.NbtPredicate;
 import net.minecraft.predicate.NumberRange;
@@ -8,6 +9,6 @@ import net.minecraft.predicate.item.ItemPredicate;
 
 public class SimpleItemPredicate extends ItemPredicate {
     public SimpleItemPredicate(Item item) {
-        super(null, item, NumberRange.IntRange.ANY, NumberRange.IntRange.ANY, EnchantmentPredicate.ARRAY_OF_ANY, EnchantmentPredicate.ARRAY_OF_ANY, null, NbtPredicate.ANY);
+        super(null, ImmutableSet.of(item), NumberRange.IntRange.ANY, NumberRange.IntRange.ANY, EnchantmentPredicate.ARRAY_OF_ANY, EnchantmentPredicate.ARRAY_OF_ANY, null, NbtPredicate.ANY);
     }
 }
