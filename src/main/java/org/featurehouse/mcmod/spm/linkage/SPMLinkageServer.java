55f6efc7bb1a57d0e301654d697962a9df67c8bf
Teddy Li <teddyxlandlee@hotmail.com>
2021-08-10 15:27:27 +0800

    Server Linkage

--- src/main/java/org/featurehouse/mcmod/spm/linkage/SPMLinkageServer.java
+++ src/main/java/org/featurehouse/mcmod/spm/linkage/SPMLinkageServer.java
@@ -2,9 +2,15 @@ package org.featurehouse.mcmod.spm.linkage;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
 
 @Environment(EnvType.SERVER)
 @FunctionalInterface
 public interface SPMLinkageServer {
     void initServer();
+
+    static void load() {
+        FabricLoader.getInstance().getEntrypoints("sweet_potato.server", SPMLinkageServer.class)
+                .forEach(SPMLinkageServer::initServer);
+    }
 }
